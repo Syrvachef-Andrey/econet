46a3dad45a416ebd7a9bbf38c02c27208d6b86b7
daniel.kurushin <daniel.kurushin@gmail.com>
2020-01-16 13:13:15 +0500

    extrapolate

--- cv/gamefield.py
+++ cv/gamefield.py
@@ -2,49 +2,84 @@ import cv2 as cv
 import numpy as np
 from scipy.spatial.distance import euclidean as dist
 
-img = cv.imread('img/field-5.jpeg')
+def findLines(filename):
+    img = cv.imread(filename)
+    
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    edges = cv.Canny(gray, 50, 150, apertureSize = 3)
+    lines = cv.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=200, maxLineGap=10)
+    
+    return img, lines
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-edges = cv.Canny(gray, 50, 150, apertureSize = 3)
-lines = cv.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=200, maxLineGap=10)
-colors = np.linspace(0,255,len(lines))
-i = 0
+def getEquations(lines):
+    eqA = []
+    eqB = []
+    for line in lines:
+        x = line[0][0::2]
+        y = line[0][1::2]
+        k, b = np.polyfit(x, y, 1)
+        if k < 0:
+            eqA += [(k, b)]
+        else:
+            eqB += [(k, b)]
+    return eqA, eqB
 
-eqA = []
-eqB = []
+def filterEquations(eqQ):
+    res = []
+    for k0, b0 in eqQ:
+        nearest = []
+        for k1, b1 in eqQ:
+            if abs(k0-k1) < 1 and abs(b0-b1) < 15:
+                nearest += [(k1, b1)]
+        k, b = nearest[0]
+        res += [(k, b)]
+    return res
 
-for line in lines:
-    x = line[0][0::2]
-    y = line[0][1::2]
-    k, b = np.polyfit(x, y, 1)
-    if k < 0:
-        eqA += [(k, b)]
-    else:
-        eqB += [(k, b)]
+def extrapolate(eqQ):
+    K = [ k for k, b in eqQ]
+    B = [ b for k, b in eqQ]
+    import matplotlib.pyplot as plt
+    X = list(range(len(K)))
+    a, b, c = np.polyfit(X, sorted(K), 2)
+    K1 = [ a*x**2 + b*x +c for x in range(-2, len(X) + 1) ]
 
-eqAA = []
-for k0, b0 in eqA:
-    nearest = []
-    for k1, b1 in eqA:
-        if abs(k0-k1) < 1 and abs(b0-b1) < 15:
-            nearest += [(k1, b1)]
-    k, b = nearest[0]
-    eqAA += [(k, b)]
+    a, b, c = np.polyfit(X, sorted(B), 2)
+    B1 = [ a*x**2 + b*x +c for x in X ]
+    
+def solve(eqA, eqB):
+    points = []
+    for eqa in eqA:
+        for eqb in eqB:
+            a = [[1, -eqa[0]],[1, -eqb[0]]]
+            b = [eqa[1],eqb[1]]
+            x, y =  np.linalg.solve(a, b) 
+            points += [(x,y)]
+    filtered_points = []
+    for a in points:
+        nearest = []
+        for b in points:
+            if dist(a,b) < 20:
+                nearest += [b]
+        x, y = [ int(_) for _ in np.mean(nearest, axis=0) ]
+        filtered_points += [(x, y)]
+    return filtered_points
 
-eqBB = []
-for k0, b0 in eqB:
-    nearest = []
-    for k1, b1 in eqB:
-        if abs(k0-k1) < 1 and abs(b0-b1) < 15:
-            nearest += [(k1, b1)]
-    k, b = nearest[0]
-    eqBB += [(k, b)]
-
-for eq in eqAA + eqBB:
-    k, b = eq
-    y0 = int(k *   0 + b)
-    y1 = int(k * 640 + b)
-    cv.line(img, (0, y0), (640, y1), (0,255,0), 1)
+if __name__ == '__main__':
+    img, lines = findLines('img/field-5.jpeg')
+    eqA, eqB = [ filterEquations(eq) for eq in getEquations(lines) ]
+    eqA = extrapolate(eqA)
+#    raise Exception
+    points = solve(eqA, eqB)
+    for eq in eqA + eqB:
+        k, b = eq
+        y0 = int(k *   0 + b)
+        y1 = int(k * 640 + b)
+        cv.line(img, (0, y0), (640, y1), (0,255,0), 1)
+    for x, y in points:
+        cv.circle(img, (int(y), int(x)), 5, (0,255,0), -1)    
+    cv.imshow('image',img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 #points = []
 #for eqa in eqA:
 #    for eqb in eqB:
@@ -54,17 +89,6 @@ for eq in eqAA + eqBB:
 #        points += [(x,y)]
 ##        cv.circle(img, (y, x), 2, (0,255,0), -1)
 #
-#filtered_points = []
-#for a in points:
-#    nearest = []
-#    for b in points:
-#        if dist(a,b) < 20:
-#            nearest += [b]
-#    x, y = [ int(_) for _ in np.mean(nearest, axis=0) ]
-#    filtered_points += [(x, y)]
-#    cv.circle(img, (y, x), 2, (0,255,0), -1)
+#    
     
     
-cv.imshow('image',img)
-cv.waitKey(0)
-cv.destroyAllWindows()
